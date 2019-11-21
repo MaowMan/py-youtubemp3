c9fb0f7eebe2a3f1042002713ced84cbd1cbe29f
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-11-21 22:16:32 +0800

    0.1

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,12 @@
-from subprocess import run,PIPE
+from subprocess import  run,PIPE
 
-process=run(["youtube-dl"],stdout=PIPE)
-print(process.stdout)
+def download(url,args):
+    if type(url)==str:
+        url=[url]
+    process=run((["python","-m","youtube_dl"]+url+args),stdout=PIPE,stderr=PIPE)
+    return (process.stdout.format(),process.stderr.format())
+
+
+if __name__=="__main__":
+    print(download('https://www.youtube.com/watch?v=VpwAq7hiij0',["-x","--audio-format","wav"]))
+
