80e306ae4d698e58389436f074c0d0e36502967c
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-11-22 22:27:58 +0800

    0.1

--- Scripts/gui.py
+++ Scripts/gui.py
@@ -1,2 +1,16 @@
 from tkinter import *
 
+class Main(object):
+    def __init__(self):
+        self.Root=Tk()
+        self.Root.title("py-youtubemp3")
+        self.Setup()
+        self.Loop()
+    def Setup(self):
+        self.Topbar=Menu(self.Root)
+        self.Root.config(menu=self.Topbar)
+    def Loop(self):
+        self.Root.mainloop()
+
+if __name__=="__main__":
+    Main()
